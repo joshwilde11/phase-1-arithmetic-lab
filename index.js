016e9a144359a87d1af2e3b2facce848b8c6ad37
Josh Wilde <jowilde11@gmail.com>
2024-11-19 00:25:48 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10) + 1;
+
+// 25/7
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+
+let numbers = [5, 10, 15, 20];
+let max = Math.max(...numbers);
